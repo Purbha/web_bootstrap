d4e68877295b8394b7970aa5f0baacf78882e8aa
Irsyad <irsyadpurbha@gmail.com>
2021-01-16 16:11:10 +0700

    UPDATE 20210116 16:11

--- Pertemuan1/Lat1.php
+++ Pertemuan1/Lat1.php
@@ -13,7 +13,7 @@
 <div class="container">
 	<h1>Latihan 1</h1>
 	<p>Halaman bootstrap pertama. Ini adalah contoh paragraf sederhana!</p>
-	<p>Selamat Datang <kbd>Irsyad</kbd></p>
+	<p>Selamat Datang <kbd>Irsyad Purbha</kbd></p>
 </div>
 </body>
 </html> 
